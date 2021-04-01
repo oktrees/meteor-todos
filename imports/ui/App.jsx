12855978e69b7e8cf59fc7a1afea0144bb1a2284
ok <ok@oghyangmog-ui-MacBookPro.local>
2021-04-01 15:44:20 +0900

    first commit

---
+++ imports/ui/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Task } from './Task.jsx';
+
+const tasks = [
+  {_id: 1, text: 'First Task'},
+  {_id: 2, text: 'Seconf Task'},
+  {_id: 3, text: 'Third Task'},
+];
+
+export const App = () => (
+  <div>
+    <h1>Welcome to Meteor!</h1>
+    <ul>
+      { tasks.map(task => <Task key={ task._id } task={ task }/>) }
+    </ul>
+  </div>
+);
