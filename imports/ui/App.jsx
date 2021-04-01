e8b63e7bb1b965016177bd0a29d2c8ccac44dc94
ok <ok@oghyangmog-ui-MacBookPro.local>
2021-04-01 16:52:18 +0900

    create addTodoForm

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { useTracker } from 'meteor/react-meteor-data';
+import { TasksCollection } from '/imports/api/TasksCollection';
 import { Task } from './Task.jsx';
+import { TaskForm } from './TaskForm';
 
 const tasks = [
   {_id: 1, text: 'First Task'},
@@ -7,11 +10,18 @@ const tasks = [
   {_id: 3, text: 'Third Task'},
 ];
 
-export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <ul>
-      { tasks.map(task => <Task key={ task._id } task={ task }/>) }
-    </ul>
-  </div>
-);
+export const App = () => {
+  const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch());
+
+  return (    
+    <div>
+      <h1>Welcome to Meteor!</h1>
+
+      <TaskForm/>
+
+      <ul>
+        { tasks.map(task => <Task key={ task._id } task={ task }/>) }
+      </ul>
+    </div>
+  )
+};
