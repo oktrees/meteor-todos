1f5a7a9e60670c4121ccbcfa88ba19b240c9104c
ok <ok@oghyangmog-ui-MacBookPro.local>
2021-04-01 18:01:58 +0900

    add todos tools(filter,delete,completed)

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,27 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
 import { TasksCollection } from '/imports/api/TasksCollection';
 import { Task } from './Task.jsx';
 import { TaskForm } from './TaskForm';
 
-const tasks = [
-  {_id: 1, text: 'First Task'},
-  {_id: 2, text: 'Seconf Task'},
-  {_id: 3, text: 'Third Task'},
-];
+const toggleChecked = ({ _id, isChecked }) => {
+  TasksCollection.update(_id, {
+    $set: {
+      isChecked: !isChecked
+    }
+  })
+};
+
+const deleteTask = ({ _id }) => TasksCollection.remove(_id);
 
 export const App = () => {
-  const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch());
+  const [hideCompleted, setHideCompleted] = useState(false);  
+  const hideCompletedFilter = { isChecked: { $ne: true } };
+  const tasks = useTracker(() => TasksCollection.find(hideCompleted ? hideCompletedFilter : {}, { 
+    sort: { createdAt: -1 } 
+  }).fetch());
+  const pendingTasksCount = useTracker(() => 
+    TasksCollection.find(hideCompletedFilter).count()
+  );
+
+  const pendingTasksTitle = `${
+    pendingTasksCount ? `(${pendingTasksCount})` : ''
+  }`;
 
   return (    
-    <div>
-      <h1>Welcome to Meteor!</h1>
+    <div className="app">
+      <header>
+        <div className="app-bar">
+          <div className="app-header">
+            <h1>📝️ To Do List</h1>
+            {pendingTasksTitle}
+          </div>
+        </div>
+      </header>
 
-      <TaskForm/>
+      <div className="main">
+        <TaskForm />
 
-      <ul>
-        { tasks.map(task => <Task key={ task._id } task={ task }/>) }
-      </ul>
+        <div className="filter">
+          <button onClick={() => setHideCompleted(!hideCompleted)}>
+            {hideCompleted ? 'Show All' : 'Hide Completed'}
+          </button>
+        </div>
+
+        <ul className="tasks">
+          {tasks.map(task => (
+            <Task
+              key={task._id}
+              task={task}
+              onCheckboxClick={toggleChecked}
+              onDeleteClick={deleteTask}
+            />
+          ))}
+        </ul>
+      </div>
     </div>
   )
 };
